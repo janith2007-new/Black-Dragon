8271277c4937434c3c10bef9fb9002aafdbf51ec
janith2007-new <119475407+janith2007-new@users.noreply.github.com>
2023-01-25 17:24:14 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,23 +14,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['94786802371'] //ur owner number
-global.ownername = "Nimesh Official" //ur owner name
-global.ytname = "YT: HELA CLOUD" //ur yt chanel name
-global.socialm = "GitHub: nimaofficial" //ur github or insta name
+global.owner = ['94727785006'] //ur owner number
+global.ownername = "ᴍͥ ʀ  ᴊ ᴀᷧɴⷨ ɪ ⁰⁰⁶ 🕵🏻‍♂️🔨 Official" //ur owner name
+global.ytname = "YT: DKZ MODZ" //ur yt chanel name
+global.socialm = "GitHub: janith2007-new" //ur github or insta name
 global.location = "Sri Lanka, Colombo, Kiribathgoda" //ur location
 
 //bot bomdy 
-global.ownernomer = "94786802371" //ur number
-global.premium = ['94786802371'] //ur premium number
-global.botname = 'BLACK DRAGON' //ur bot name
-global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
-global.websitex = "https://www.helacloud.ga" //ur website to be displayed
-global.botscript = 'https://github.com/nimaofficial/Black-Dragon' //script link
-global.themeemoji = "❤️" //ur theme emoji
-global.packname = "Sticker By Black Dragon" //ur sticker watermark packname
-global.author = "Nimesh Official" //ur sticker watermark author
-global.wm = "𝗕𝗟𝗔𝗖𝗞 𝗗𝗥𝗔𝗚𝗢𝗡 𝗜𝗡𝗖" //ur watermark
+global.ownernomer = "94727785006" //ur number
+global.premium = ['94727785006'] //ur premium number
+global.botname = '❝𝘗𝘙𝘐𝘕𝘊𝘌𝘚🪄𝓙🖇️𝓚 ❞ ' //ur bot name
+global.linkz = "http://tiktok.com/@carlovers28official" //your theme url which will be displayed on whatsapp
+global.websitex = "http://tiktok.com/@carlovers28official" //ur website to be displayed
+global.botscript = 'https://github.com/janith2007-new/Black-Dragon' //script link
+global.themeemoji = "🤫" //ur theme emoji
+global.packname = "Sticker By ᴍͥ ʀ  ᴊ ᴀᷧɴⷨ ɪ ⁰⁰⁶ 🕵🏻‍♂️🔨" //ur sticker watermark packname
+global.author = "ᴍͥ ʀ  ᴊ ᴀᷧɴⷨ ɪ ⁰⁰⁶ 🕵🏻‍♂️🔨" //ur sticker watermark author
+global.wm = " ❝𝘗𝘙𝘐𝘕𝘊𝘌𝘚🪄𝓙🖇️𝓚 ❞" //ur watermark
 
 // Other
 global.sessionName = 'session'
